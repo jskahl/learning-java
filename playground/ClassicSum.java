49328c4b7e13f024cbda2fd98849a2c1cc6d074a
JSkahl <josekahl333@gmail.com>
2025-11-26 23:26:17 -0300

    first commit

---
+++ playground/ClassicSum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class ClassicSum {
+    public static void main(String[] args) {
+        String greeting = "Hello, world (this is a variable)!";
+        System.out.println(greeting);
+
+        System.out.println("-----------------------");
+
+        Integer a = 5;
+        Integer b = 5;
+        Integer sum = a + b;
+
+        System.out.println("a: " + a + " and b: " + b);
+        System.out.println("This will print the variable sum");
+        System.out.println("So, a + b = " + sum);
+
+        System.out.println("This will print the variable a plus the variable b");
+        System.out.println("So, a + b = " + (a + b));
+    } 
+}
---
+++ playground/Input.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main() {
+        System.out.println("-----------------------------");
+        System.out.println("------------Name-------------");
+        System.out.println("-----------------------------");
+
+        String greeting = "Welcome, ";
+
+        Scanner firstInput = new Scanner(System.in);
+        System.out.println("Enter first name: ");
+        String firstName = firstInput.nextLine();
+
+        Scanner secondInput = new Scanner(System.in);
+        System.out.println("Enter second name: ");
+        String secondName = secondInput.nextLine();
+
+        System.out.println(greeting + firstName + " " + secondName + "!");
+
+        System.out.println("-----------------------------");
+        System.out.println("-------------Sum-------------");
+        System.out.println("-----------------------------");
+
+        Scanner aInput = new Scanner(System.in);
+        System.out.println("Enter first value: ");
+        Integer a = aInput.nextInt();
+
+        Scanner bInput = new Scanner(System.in);
+        System.out.println("Enter second value: ");
+        Integer b = bInput.nextInt();
+
+        System.out.println("a: " + a + " and b: " + b);
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
