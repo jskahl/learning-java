4b9c2601654f9139872e1ecf3655f64f2bc10d5e
JSkahl <josekahl333@gmail.com>
2025-11-28 17:43:48 -0300

    adjust structure

--- playground/Class.java
+++ playground/Class.java
@@ -22,3 +22,13 @@ public class Person {
         );
     }
 }
+
+public class Main {
+   public static void main(String[] args) {
+        Person person1 = new Person("José", "Kahl", 19);
+        Person person2 = new Person();
+
+        person1.printInfo();
+        person2.printInfo();
+   } 
+}
--- playground/Classes/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-   public static void main(String[] args) {
-        Person person1 = new Person("José", "Kahl", 19);
-        Person person2 = new Person();
-
-        person1.printInfo();
-        person2.printInfo();
-   } 
-}
