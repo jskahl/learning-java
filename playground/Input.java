724bbda9d47ca7cbc3350bcfce87896efd4f507b
JSkahl <josekahl333@gmail.com>
2025-11-27 21:05:29 -0300

    correct main method parameters

--- playground/Input.java
+++ playground/Input.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Input {
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println("-----------------------------");
         System.out.println("------------Name-------------");
         System.out.println("-----------------------------");
